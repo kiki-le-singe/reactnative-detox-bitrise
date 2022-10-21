435e7058a6a0cbcd9990e4bcc4300d5285c8ba69
anthony <aalbertini95@gmail.com>
2022-10-21 20:16:53 +0200

    remove Pods from repo

--- ios/Pods/Target Support Files/Pods-BitriseRNDetox-BitriseRNDetoxTests/Pods-BitriseRNDetox-BitriseRNDetoxTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BitriseRNDetox_BitriseRNDetoxTests : NSObject
-@end
-@implementation PodsDummy_Pods_BitriseRNDetox_BitriseRNDetoxTests
-@end
